a5be003c93d902788e441cf181a79b9489482e6f
mellanspel <161259219+mellanspel@users.noreply.github.com>
2024-03-28 17:10:12 -0400

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
 
-// Your bot token obtained from Discord Developer Portal
-const TOKEN = config.token;
+const TOKEN = process.env.TOKEN;
 
-// List of messages the bot can choose from
+// msg
 const messages = [
     "Hello!",
     "How are you?",
@@ -13,27 +11,24 @@ const messages = [
     "Hey there!"
 ];
 
-// Create a new Discord client
 const client = new Discord.Client();
 
-// Event: Bot is ready
 client.once('ready', () => {
     console.log('Bot is ready!');
 });
 
-// Event: Message received
 client.on('message', message => {
-    // Ignore messages from the bot itself
+    // Ignore messages from itself
     if (message.author.bot) return;
 
-    // Select a random message from the list
+    // random
     const randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
-    // Send the random message to the same channel where the original message was received
+    //channel
     message.channel.send(randomMessage)
         .then(() => console.log(`Message sent in ${message.channel.name}: ${randomMessage}`))
         .catch(error => console.error('Error sending message:', error));
 });
 
-// Log in to Discord with your app's token
+// token
 client.login(TOKEN);
